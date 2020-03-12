c0c7f53802636a7e6e275130cc3cdf8e91e0b035
Julio Souza <jcs.caesar@gmail.com>
2020-03-12 14:09:19 -0300

    Alteracoes feitas ate o fim do modulo 2.6

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,11 +2,25 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Entity;
+import org.hibernate.Hibernate;
+
+@Entity
 public class Sessao {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
 	
 	public Sessao (LocalTime horario, Filme filme, Sala sala) {
@@ -46,5 +60,11 @@ public class Sessao {
 
 	public void setFilme(Filme filme) {
 		this.filme = filme;
+	}
+	
+	/**
+	 *  @deprecated hibernate only
+	 */
+	public Sessao() {
 	}
 }
